ec2227fc8a6251c62ac3f55ee903ab5b92f04095
Pedro Rocha Horchulhack <17484454+hrchlhck@users.noreply.github.com>
2026-07-03 17:51:54 +0000

    ADicionado atualizar_tarefa e criar_tarefa

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ def nova_tarefa(id: int, titulo: str, descricao: str):
         "criado_em": datetime.now()
     }
 
+def verificar_existencia_tarefa(id: int):
+    """Função auxiliar para verificar a existência de uma tarefa com base no seu ID"""
+    for tarefa in LISTA_TAREFAS:
+        if id == tarefa['id']:
+            return True
+    return False
+
 @APP.get("/")
 def index():
     return "Olá, DevOps!"
 
 @APP.get("/tarefas")
 def listar_tarefas():
-    global LISTA_TAREFAS
-
     # Lista tarefas (somente id e titulo)
     if len(LISTA_TAREFAS) == 0:
         return LISTA_TAREFAS
@@ -45,3 +50,78 @@ def listar_tarefa_especifica(id: int):
         return LISTA_TAREFAS[id]
     
     return mensagem_padrao
+
+# Implementar!
+# @APP.post("/tarefas")
+# Rota /tarefas (POST)
+#   Entrada: id da tarefa (int), titulo da tarefa (str) e descrição da tarefa (str)
+#   Funcionamento:
+#       - Recebe os dados como parâmetro de requisição
+#       - Cria uma nova tarefa usando a função `nova_tarefa`
+#       - Adiciona nova tarefa a LISTA_TAREFAS
+#   # Saída:
+#       - Retorna "OK" se a tarefa foi criada
+#       - Se a tarefa existir, retornar "TAREFA JÁ EXISTE"
+
+@APP.post("/tarefas")
+def criar_tarefa(id: int, titulo: str, descricao: str):
+    global LISTA_TAREFAS
+
+    tarefa_existe = verificar_existencia_tarefa(id)
+
+    if tarefa_existe:
+        return {"mensagem": "TAREFA JÁ EXISTE!"}
+    
+    nova = nova_tarefa(id, titulo, descricao)
+
+    LISTA_TAREFAS.append(nova)
+
+    return {"mensagem": "OK"}
+
+# @APP.put("/tarefas/{id}")
+# Rota /tarefas/{id} (PUT)
+#   Entrada: id da tarefa (int), titulo da tarefa (str), descrição da tarefa (str) e concluido (bool)
+#   Funcionamento:
+#       - Recebe os dados como parâmetro de requisição
+#       - Atualiza informações da tarefa de id específico
+#   # Saída:
+#       - Retorna "OK" se a tarefa foi atualizada
+#       - Se a tarefa NÃO existir, retornar "TAREFA NÃO EXISTE"
+@APP.put("/tarefas/{id}")
+def atualizar_tarefa(id: int, titulo: str = "", descricao: str = "", concluido: bool = False):
+    global LISTA_TAREFAS
+
+    tarefa_existe = verificar_existencia_tarefa(id)
+
+    if not tarefa_existe:
+        return {"mensagem": "TAREFA NÃO EXISTE!"}
+    
+    tarefa = None
+    for indice in range(len(LISTA_TAREFAS)):
+        tarefa = LISTA_TAREFAS[indice]
+
+        # Sai do loop
+        if tarefa['id'] == id:
+            break
+    
+    if titulo != "":
+        LISTA_TAREFAS[indice]['titulo'] = titulo
+    
+    if descricao !=  "": 
+        LISTA_TAREFAS[indice]['descricao'] = descricao
+    
+    LISTA_TAREFAS[indice]['concluido'] = concluido
+
+    return {"mensagem": "OK"}
+
+# @APP.delete("/tarefas")
+# Rota /tarefas/{id} (DELETE)
+#   Entrada: id da tarefa (int)
+#   Funcionamento:
+#       - Recebe os dados como parâmetro de requisição
+#       - Busca pela tarefa com base no ID
+#       - Se tarefa existir, remover de LISTA_TAREFAS
+#       - Se NÃO existir, retorna "TAREFA NÃO EXISTE"
+#   # Saída:
+#       - Retorna "OK" se a tarefa foi removida
+#       - Se a tarefa NÃO existir, retornar "TAREFA NÃO EXISTE"
